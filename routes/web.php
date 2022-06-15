8dd3730832c3f59f12a89304f4b489f7a00a79a4
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-06-15 11:06:48 +0800

    wip user management

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -34,3 +35,6 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
+Route::resource('users', UserController::class);
